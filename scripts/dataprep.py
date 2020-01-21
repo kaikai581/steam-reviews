25b0e3bbec73c1ee8ec24b4ac26cf6d90be56422
Shih-Kai Lin <kaikai5816787@gmail.com>
2020-01-21 17:05:42 -0600

    Code for importing data.

--- scripts/dataprep.py
+++ scripts/dataprep.py
@@ -1,10 +1,10 @@
-import os
+from pathlib import Path
 import pandas as pd
 
 class GameReviews:
     def __init__(self, data_path):
         self.df = pd.read_json(data_path, lines=True)
-        self.df.insert(0, 'title', os.path.splitext(os.path.basename(data_path))[0])
+        self.df.insert(0, 'title', Path(data_path).stem)
 
 def main():
     arma3_revs = GameReviews('../data/Arma_3.jsonlines')
