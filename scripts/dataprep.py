431467e6e9743ae992c1af508cd0269f613d2929
unknown <kaikai581@hotmail.com>
2020-01-22 01:17:54 -0600

    Work in progress.

--- scripts/dataprep.py
+++ scripts/dataprep.py
@@ -5,11 +5,14 @@ class GameReviews:
     def __init__(self, data_path):
         self.df = pd.read_json(data_path, lines=True)
         self.df.insert(0, 'title', Path(data_path).stem)
+        # For simplicity, replace missing values with 0.
+        self.df = self.df.fillna(0)
 
 def main():
     arma3_revs = GameReviews('../data/Arma_3.jsonlines')
     pd.set_option('display.max_columns', None)
     print(arma3_revs.df.head())
+    print('\nColumns containing missing values:')
     print(arma3_revs.df.isna().any())
 
 
